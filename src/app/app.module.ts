4e39f1b181ebf02b6180189dd99871ced94803ca
Miguel <llobellmiguel03@gmail.com>
2023-04-26 09:47:21 +0200

    Initiar commit with index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule,Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { EntradasComponent } from './components/entradas/entradas.component';
+import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
+import { TrabajaConNosotrosComponent } from './components/trabaja-con-nosotros/trabaja-con-nosotros.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+const Route:Routes=[ 
+  {path:'',component:InicioComponent},
+  {path:'entradas',component:EntradasComponent},
+  {path:'sobre-nosotros',component:SobreNosotrosComponent},
+  {path:'trabaja-con-nosostros',component:TrabajaConNosotrosComponent},
+  {path:'contacto',component:ContactoComponent},
+
+  
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    EntradasComponent,
+    SobreNosotrosComponent,
+    TrabajaConNosotrosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(Route)
   ],
   providers: [],
   bootstrap: [AppComponent]
