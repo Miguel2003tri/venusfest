2c8feff4326850d021f5140a4d8b9dfbbdf98b16
Miguel <llobellmiguel03@gmail.com>
2023-05-16 21:08:07 +0200

    contacto validate form

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -7,25 +7,57 @@ import { Router } from '@angular/router';
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
-export class ContactoComponent implements OnInit{
+export class ContactoComponent implements OnInit {
   @Input() contacto = {
     nombre: '',
     telf: '',
     email: '',
-    asunto: '', 
+    asunto: '',
     mensaje: '',
   }
   modelo: any = {};
 
-  constructor(private http: MyApiService, public router: Router) {}
-  ngOnInit(): void {}
+  constructor(private http: MyApiService, public router: Router) { }
+  ngOnInit(): void { }
 
   enviarDatosContacto() {
-    this.http.createContacto(this.contacto)
-    .subscribe((data) => {
-      this.router.navigate(['/'])
-    })
+    let logError = 0
+    const regexTelf = /^\d{9}$/;
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    console.log(this.contacto.nombre);
     
+
+    if ( this.contacto.nombre === "") {
+      logError++
+    }
+    if (this.contacto.telf==="" ||regexTelf.test(this.contacto.telf)) {
+      logError++
+    } 
+    if ( this.contacto.email === "" || regexEmail.test(this.contacto.email)) {
+      logError++
+    } 
+    if (this.contacto.asunto === "") {
+      logError++
+    } 
+    if (this.contacto.mensaje === "") {
+      logError++
+    }
+    console.log(logError);
     
+    if (logError == 0) {
+      console.log("Dins");
+
+      this.http.createContacto(this.contacto)
+        .subscribe((data) => {
+          this.router.navigate(['/'])
+        })
+    }
+     //alert div
+
   }
+
+
+
+
 }
