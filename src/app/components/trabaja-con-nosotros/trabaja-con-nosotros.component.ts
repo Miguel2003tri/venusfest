2198cd64ce909e3ac1a42599d633b682fed8b326
Miguel <llobellmiguel03@gmail.com>
2023-05-16 21:07:09 +0200

    validate components in forms

--- src/app/components/trabaja-con-nosotros/trabaja-con-nosotros.component.ts
+++ src/app/components/trabaja-con-nosotros/trabaja-con-nosotros.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   templateUrl: './trabaja-con-nosotros.component.html',
   styleUrls: ['./trabaja-con-nosotros.component.css'],
 })
-export class TrabajaConNosotrosComponent implements OnInit  {
+export class TrabajaConNosotrosComponent implements OnInit {
   @Input() artista = {
     nombre: '',
     nombre_artistico: '',
@@ -25,15 +25,59 @@ export class TrabajaConNosotrosComponent implements OnInit  {
   }
   modelo: any = {};
 
-  constructor(private http: MyApiService, public router: Router) {}
-  ngOnInit(): void {}
+  constructor(private http: MyApiService, public router: Router) { }
+  ngOnInit(): void { }
 
   enviarDatosNosotros() {
-    this.http.createArtista(this.artista)
-    .subscribe((data) => {
-      this.router.navigate(['/'])
-    })
-    
-    
+    let logError = 0
+    const regexTelf = /^\d{9}$/;
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regexLink = /^(ftp|http|https):\/\/[^ "]+$/;
+
+    if (this.artista.nombre === "") {
+      logError++
+    }
+    if (this.artista.nombre_artistico === "") {
+      logError++
+    }
+    if (this.artista.email === "" || regexEmail.test(this.artista.email)) {
+      logError++
+    }
+    if (this.artista.telf === ""||regexTelf.test(this.artista.telf)) {
+      logError++
+    }
+    if (this.artista.direccion_postal === "") {
+      logError++
+    }
+    if (this.artista.descripcion_proyecto === "") {
+      logError++
+    }
+    if (this.artista.enlaces_redes_sociales === "" || regexLink.test(this.artista.enlaces_redes_sociales)) {
+      logError++
+    }
+    if (this.artista.experiencia_previa === "") {
+      logError++
+    }
+    if (this.artista.requerimientos_tecnicos === "") {
+      logError++
+    }
+    if (this.artista.disponibilidad === "") {
+      logError++
+    }
+    if (this.artista.condiciones_participacion === "") {
+      logError++
+    }
+//alert div
+
+
+    console.log(logError);
+    if (logError == 0) {
+      this.http.createArtista(this.artista)
+        .subscribe((data) => {
+          this.router.navigate(['/'])
+        })
+
+    }
+
   }
 }
